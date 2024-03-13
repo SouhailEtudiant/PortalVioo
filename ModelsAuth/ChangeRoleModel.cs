91c9c374efa6d2da152272baae5d00c66cff3b29
SouhailEtudiant <Souhaiel.Hajri@etudiant-isi.utm.tn>
2024-03-13 02:54:41 +0100

    Ajouter des fonctionnalités de gestion des utilisateurs

---
+++ ModelsAuth/ChangeRoleModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniProjectBack.ModelsAuth
+{
+    public class ChangeRoleModel
+    {
+        public string username { get; set; }
+  
+        public string role { get; set; }
+    }
+}
---
+++ ModelsAuth/getuserwthrole.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniProjectBack.ModelsAuth
+{
+    public class getuserwthrole
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string RoleId { get; set; }
+    }
+}
