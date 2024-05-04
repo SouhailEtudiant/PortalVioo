84e84a7a4ebf4206a6310fb0a331d6c4c06b5410
SouhailEtudiant <Souhaiel.Hajri@etudiant-isi.utm.tn>
2024-05-05 00:20:12 +0100

    commit

--- DTO/MembreProjetDTO.cs
+++ DTO/MembreProjetDTO.cs
@@ -13,6 +13,9 @@ namespace PortalVioo.ModelsApp
         public string IdUtilisateur { get; set; }
         public int IdProjet { get; set; }
         public string username { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string imgpath { get; set; }
         public string TitreProjet { get; set; }
         public string roleMembre { get; set; }
 
---
+++ ModelsApp/imputationAdd.cs
@@ -0,0 +1,22 @@
+﻿using PortalVioo.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalVioo.ModelsApp
+{
+    public class imputationAdd
+    {
+     //   public int Id { get; set; }
+
+        public string date { get; set; }
+
+        public decimal chargeEnHeure { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string IdUtilisateur { get; set; }
+
+        public int IdTache { get; set; }
+
+    }
+}
